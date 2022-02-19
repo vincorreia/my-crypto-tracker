98baa750c8531c738a179aa81c9905bf72d57074
vincorreia <vcorreiaes@gmail.com>
2022-02-19 18:25:59 +0100

    Add refresh every 2 minutes to currency page

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import CurrencyPage from "../views/routes/CurrencyPage/CurrencyPage"
+
 export default function Content(props:{}) {
     return(
         <Routes>
-            <Route path="/" element={<CurrencyPage />}/>
+            <Route path="/" element={<CurrencyPage id={90} />}/>
         </Routes>
     )
 }
--- src/views/routes/CurrencyPage/CurrencyPage.tsx
+++ src/views/routes/CurrencyPage/CurrencyPage.tsx
@@ -1,10 +1,16 @@
 import React, {useState, useEffect} from "react";
 import "./Currency.css"
 import axios from "axios";
-import Currency from "../../../interfaces/currency"
 
-export default function CurrencyPage() {
-    const url = "https://api.coinlore.net/api/ticker/?id=90";
+type Props = {
+    id: number;
+}
+export default function CurrencyPage(props: Props) {
+
+    const currencyId = String(props.id);
+
+    const url = "https://api.coinlore.net/api/ticker/?id=" + currencyId;
+    const photoUrl = "https://cryptoicon-api.vercel.app/api/icon/"
 
     const [currency, setCurrency] = useState({
         id: "",
@@ -16,34 +22,54 @@ export default function CurrencyPage() {
         photo: "",
     })
 
+    const [refresh, setRefresh] = useState(0);
+
+
+    function refresher(): void{
+        setTimeout(()=> {
+            console.log("refreshed!")
+            setRefresh(refresh + 1);
+        }, 120000)
+    }
     useEffect(() => {
         axios.get(url).then(result => {
             const data = result.data[0];
             const mkt_cap = Number(data.market_cap_usd).toLocaleString();
+            const price = Number(data.price_usd).toLocaleString();
             setCurrency({
                 id: data.id,
                 market_cap_usd: mkt_cap,
                 name: data.name,
-                price_usd: data.price_usd,
+                price_usd: price,
                 rank: data.rank,
                 symbol: data.symbol,
-                photo: ""
+                photo: photoUrl + data.symbol.toLowerCase()
             })
         })
-    }, [])
+        refresher();
+    
+    }, [refresh])
 
     return (
         <div className="currency flex-row space-evenly">
-            <div className="currency-w100 flex-row align-flex-end">
-                <div className="currency-w80">
-                    <h1>{currency.name !== "" ? currency.name : "Loading..."}</h1>
-                    <h2>{currency.id !== "" ? "#" + currency.id : null}</h2>
+        { currency.name === "" ? <h1>Loading...</h1> : 
+        <React.Fragment>
+                <div className="currency-w100 flex-row align-flex-end">
+                    <div className="currency-w80 flex-row space-evenly">
+                        <img src={currency.photo} alt={currency.name}/>
+                    <div>
+                        <h1>{currency.name}</h1>
+                        <h2> {"#" + currency.rank}</h2>
+                    </div>
                 </div>
             </div>
-                <div className="flex-row align-flex-start currency-w100">
-                    <h3>{currency.market_cap_usd !== "" ? "Market Cap: $ " + currency.market_cap_usd : null}</h3>
-                    
-                </div>
+                <div className="flex-row justify-center currency-w100">
+                    <div>
+                    <h3>{"Market Cap: $ " + currency.market_cap_usd}</h3>
+                    <h3>{"Price: $ " + currency.price_usd}</h3>
+                    </div>
+                </div>  
+        </React.Fragment>}
         </div>
     )
 }
